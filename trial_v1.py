627210837e95427d0fff217e107bdfbfe73f3f2d
shami09 <shamilikhite@gmail.com>
2024-06-27 21:38:25 -0500

    added comment

--- trial_v1.py
+++ trial_v1.py
@@ -1,3 +1,5 @@
+#Trial version to use this package
+
 from video_audio_recorder.recorder import record_video_and_audio
 
 record_video_and_audio(video_path='my_video.mp4', audio_path='output_audio.wav', output_path='merged_video.mp4', record_time=10)
